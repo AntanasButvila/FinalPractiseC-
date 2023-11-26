a599c5a224d245e78aa8063cb7e312dad0d52878
Butvila_Antanas <antanas.werwolf@gmail.com>
2023-11-26 12:18:19 +0300

    Write programm #64

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> "1, 2, 3, 4, 5"
+// M = 4; N = 8. -> "4, 6, 7, 8"
+
+// Console.WriteLine("Ведите значение М: ");
+// int M = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Ведите значение N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// string PrintNumbers(int start, int end)
+// {
+//     if(start < 0 || end < 0)
+   
+//    return ("Вы ввели не нантуральное число!!! Попробуйте снова.").ToString();
+       
+    
+//     if(start == end) return start.ToString();
+
+//     return (start + ", " + PrintNumbers(start +1, end));
+// }
+// Console.WriteLine(PrintNumbers(M,N));

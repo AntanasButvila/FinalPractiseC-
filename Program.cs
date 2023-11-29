ba616967f15d909934cd075a9d44352dc5516060
Butvila_Antanas <antanas.werwolf@gmail.com>
2023-11-29 11:36:43 +0300

    Wrote programm Ackermann

--- Program.cs
+++ Program.cs
@@ -52,3 +52,38 @@
 // }
 
 // Console.WriteLine($"Сумма натуральных элементов в промежутке от {M} до {N} равна: {PrintSumNumbers(M,N)}");
+
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 29
+
+// Console.WriteLine("Введите неотрицательное число m:");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите неотрицательное число n:");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int AckermannFunction (int m, int n)
+// {
+//     if(m < 0 || n < 0)
+//     {
+//     Console.WriteLine("Вы ввели отрицательные числа, нужны только неотрицательные.");
+//     return 0;
+//     }
+//     if (m == 0) 
+//     {
+//         return n + 1;
+//     }
+//     if (m > 0 && n == 0)
+//     {
+//          return AckermannFunction(m - 1, 1);
+//     }
+//     if (m > 0 && n > 0)
+//     {
+//          return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+//     }
+// return AckermannFunction(m, n);
+// }
+
+// Console.WriteLine($"Функция Аккермана для чисел A(m,n) = {AckermannFunction(m, n)}");
